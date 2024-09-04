e10bb773de8ae3d2ffd8d3558d29f85c62c4ae20
adoumadje <adoumadjecaleb@gmail.com>
2024-09-04 17:54:57 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomePageComponent } from "./components/patient/home-page/home-page.component";
+import { NavigationBarComponent } from "./components/general/navigation-bar/navigation-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomePageComponent, NavigationBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
