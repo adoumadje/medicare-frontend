fec36ce5e10586e30b0ce4efd92b6df0c351e9f5
adoumadje <adoumadjecaleb@gmail.com>
2024-09-14 20:51:19 +0200

    finished accordion

--- src/app/components/accordion/accordion.component.ts
+++ src/app/components/accordion/accordion.component.ts
@@ -1,5 +1,5 @@
 import { NgIf } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-accordion',
@@ -9,16 +9,15 @@ import { Component, Input } from '@angular/core';
   styleUrl: './accordion.component.css'
 })
 export class AccordionComponent {
-    active: boolean;
 
-    @Input() faq:any = {}
+    @Input() faq:any = { }
+    @Output() accordionClicked:EventEmitter<any> = new EventEmitter<any>();
 
     constructor() {
-      this.active = false;
+
     }
 
-    toggle():void {
-      this.active = !this.active;
-      console.log("clicked")
+    toggle(id:any):void {
+      this.accordionClicked.emit(id)
     }
 }
--- src/app/components/patient/home-page/home-page.component.ts
+++ src/app/components/patient/home-page/home-page.component.ts
@@ -12,24 +12,48 @@ import { NgFor } from '@angular/common';
 export class HomePageComponent {
     faqs:any[] = [
       {
+        id: 1,
         question: "How do I book an appointment with a doctor?",
-        answer: "You can easily book an appointment by browsing available doctors, selecting your preferred time slot, and confirming your appointment through our app. Once confirmed, you will receive an email or SMS notification."
+        answer: "You can easily book an appointment by browsing available doctors, selecting your preferred time slot, and confirming your appointment through our app. Once confirmed, you will receive an email or SMS notification.",
+        active: false
       },
       {
+        id: 2,
         question: "Can I cancel or reschedule an appointment?",
-        answer: "Yes, you can cancel or reschedule your appointment directly through the app by navigating to your upcoming appointments section. Please note that cancellation policies may vary depending on the doctor."
+        answer: "Yes, you can cancel or reschedule your appointment directly through the app by navigating to your upcoming appointments section. Please note that cancellation policies may vary depending on the doctor.",
+        active: false
       },
       {
+        id: 3,
         question: "Is there a fee for booking through the app?",
-        answer: "Our app is free to use for booking appointments. However, doctors may charge for their consultations, and any fees will be clearly displayed before confirming your appointment."
+        answer: "Our app is free to use for booking appointments. However, doctors may charge for their consultations, and any fees will be clearly displayed before confirming your appointment.",
+        active: false
       },
       {
+        id: 4,
         question: "How do I find a doctor based on my condition or specialty?",
-        answer: "You can search for doctors by specialty, location, or condition using our advanced search filters. This helps you find the best healthcare professional to suit your needs."
+        answer: "You can search for doctors by specialty, location, or condition using our advanced search filters. This helps you find the best healthcare professional to suit your needs.",
+        active: false
       },
       {
+        id: 5,
         question: "What types of medical services are available through the app?",
-        answer: "We provide a wide range of medical services, including general check-ups, specialist consultations, lab tests, and mental health support. Simply browse the available services in the app to find the care you need."
+        answer: "We provide a wide range of medical services, including general check-ups, specialist consultations, lab tests, and mental health support. Simply browse the available services in the app to find the care you need.",
+        active: false
       }
     ]
+
+    constructor() {
+
+    }
+
+    toggleAccordion(id:any) {
+      for(let i = 0; i < this.faqs.length; ++i) {
+        if(this.faqs[i].id === id) {
+          this.faqs[i].active = !this.faqs[i].active; 
+        } else {
+          this.faqs[i].active = false;
+        }
+      }
+    }
 }
