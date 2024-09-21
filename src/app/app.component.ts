d8ddc2deb69d6d233abfe30c1c3029739381046d
adoumadje <adoumadjecaleb@gmail.com>
2024-09-21 15:26:40 +0200

    started find a doctor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomePageComponent } from "./components/patient/home-page/home-page.component";
 import { NavigationBarComponent } from "./components/general/navigation-bar/navigation-bar.component";
+import { FindADoctorComponent } from "./components/find-a-doctor/find-a-doctor.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomePageComponent, NavigationBarComponent],
+  imports: [RouterOutlet, HomePageComponent, NavigationBarComponent, FindADoctorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
