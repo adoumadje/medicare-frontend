676185461d4ab1b52d12464202fba8890f20ad95
adoumadje <adoumadjecaleb@gmail.com>
2024-10-15 23:56:36 +0200

    reactive form

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,23 +1,25 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { pictureOrUrlValidator } from '../../validators/picture-or-url.validator';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-    registerFor:FormGroup;
+    registerForm:FormGroup;
 
     constructor() {
-        this.registerFor = new FormGroup({
+        this.registerForm = new FormGroup({
             fullname: new FormControl('', Validators.required),
             email: new FormControl('', Validators.required),
             password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+            user: new FormControl('', Validators.required),
+            gender: new FormControl('', Validators.required),
             photo: new FormGroup({
               profilePicture: new FormControl(''),
               profilePicUrl: new FormControl('')
